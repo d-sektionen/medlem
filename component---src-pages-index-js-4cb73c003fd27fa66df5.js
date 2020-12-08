d06641bd4c6cbc1240ed043a2dede5deeb3cbedb
Max <maxskan98@gmail.com>
2020-12-08 19:12:17 +0100

    Updates

--- component---src-pages-index-js-4cb73c003fd27fa66df5.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{186:function(n,e,t){"use strict";t.r(e);var a=t(0),i=t.n(a);e.default=function(){return i.a.createElement("div",null,i.a.createElement("h1",null,"Hej"),i.a.createElement("p",null,"Det finns just nu inget vettigt på startsidan."),i.a.createElement("p",null,"Använd sidomenyn, där finns vettiga saker."))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-4cb73c003fd27fa66df5.js.map
