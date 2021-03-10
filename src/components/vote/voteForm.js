5ea6c84f9a740aeb2d46ec1e4aa26ca7a08003d5
LeonEricsson <leon.ericsson@me.com>
2021-03-10 19:09:56 +0100

    Frontend v1

--- src/components/vote/voteForm.js
+++ src/components/vote/voteForm.js
@@ -2,17 +2,16 @@ import React, { Component, useState } from 'react'
 import { post } from '../request'
 
 const VoteForm = ({ vote }) => {
-  vote.number_of_selectable_alternatives = 2
-  const multipleChoice = vote.number_of_selectable_alternatives > 1
-  const [checkedId, setCheckedId] = useState(multipleChoice ? undefined : -1)
+  //vote.number_of_selectable_alternatives = 2
+  //const [checkedId, setCheckedId] = useState(multipleChoice ? undefined : -1)
   const [successfullyVoted, setSuccessfullyVoted] = useState(false)
-  const [checkedIds, setCheckedIds] = useState(multipleChoice ? [] : undefined)
+  const [checkedIds, setCheckedIds] = useState([])
 
   const placeVote = async () => {
     const voteData = {
       vote_id: vote.id,
-      alternative_id: checkedId,
-      alternative_ids: checkedIds,
+      alternative_id: checkedIds,
+      //alternative_ids: checkedIds,
     }
 
     console.log(voteData)
@@ -20,9 +19,22 @@ const VoteForm = ({ vote }) => {
     await post('/voting/made_votes/', voteData)
     setSuccessfullyVoted(true)
   }
-  console.log(checkedIds)
-  const votingDisabled = checkedId === -1
-  const buttonText = votingDisabled ? 'Välj ett alternativ' : 'Rösta'
+
+  function handleCheckboxClick(e, id) {
+    if (e.target.checked) {
+      setCheckedIds(prevState => {
+        checkedIds: [...prevState.checkedIds, id]
+      })
+    } else {
+      setCheckedIds(checkedIds.splice(id))
+    }
+  }
+
+  const multipleChoice = vote.number_of_selectable_alternatives > 1
+  const votingDisabled = checkedIds.length == 0
+  const buttonText = votingDisabled
+    ? `Välj ett (eller flera) alternativ`
+    : 'Rösta'
   const alreadyVotedText = successfullyVoted
     ? 'Tack för din röst!'
     : 'Du har röstat i omröstningen.'
@@ -41,17 +53,22 @@ const VoteForm = ({ vote }) => {
                   {multipleChoice ? (
                     <input
                       type="checkbox"
-                      onChange={() =>
+                      /*onClick={() =>
                         setCheckedIds(prevState => {
                           checkedIds: [...prevState.checkedIds, id]
                         })
-                      }
+                      }*/
+                      onClick={e => handleCheckboxClick(e, id)}
                     />
                   ) : (
                     <input
                       type="radio"
                       checked={checkedId === id}
-                      onChange={() => setCheckedId(id)}
+                      onChange={() =>
+                        setCheckedIds(prevState => {
+                          checkedIds: [...prevState.checkedIds, id]
+                        })
+                      }
                     />
                   )}
 
