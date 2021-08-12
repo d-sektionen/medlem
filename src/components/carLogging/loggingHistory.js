3d20db23d96e6124501f394d39626b3eec2de474
Max <55145161+maxska@users.noreply.github.com>
2021-08-12 16:24:02 +0200

    added finished loggings to LoggingHistory page

--- src/components/carLogging/loggingHistory.js
+++ src/components/carLogging/loggingHistory.js
@@ -10,6 +10,7 @@ import style from '../../scss/loggingHistory.module.scss'
 const LoggingHistory = () => {
   // get all logs logged by user or for booking by user
   const { data: logStarts } = useSWR('/carlogging/starts/')
+  const { data: logEnds } = useSWR('/carlogging/entries/')
 
   // console.log(logStarts)
 
@@ -37,10 +38,14 @@ const LoggingHistory = () => {
                         ? setOpenHistoryItem(null)
                         : setOpenHistoryItem(index)
                     }
-                    style={openHistoryItem ? { backgroundColor: '#333' } : {}}
+                    style={
+                      openHistoryItem === index
+                        ? { backgroundColor: '#333' }
+                        : {}
+                    }
                   >
                     <span>
-                      {`Påbörjad loggning för ${
+                      {`Påbörjad körning för ${
                         logStart.booking_user.pretty_name
                       }`}
                     </span>
@@ -63,6 +68,18 @@ const LoggingHistory = () => {
                         </div>
 
                         <div>
+                          <span>Loggning gjord</span>
+                          <span>
+                            {` ${logStart.logging_date.split('T')[0]}, kl
+                              ${
+                                logStart.logging_date
+                                  .split('T')[1]
+                                  .split('.')[0]
+                              }`}
+                          </span>
+                        </div>
+
+                        <div>
                           <span>Mätarställning</span>
                           <span>{` ${logStart.start_km} km`}</span>
                         </div>
@@ -78,6 +95,119 @@ const LoggingHistory = () => {
                             {` ${logStart.start_car_cleaned ? 'Ja' : 'Nej'}`}
                           </span>
                         </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              )
+          )}
+
+        {logEnds &&
+          logEnds.map(
+            (logEnd, index) =>
+              true && (
+                <div>
+                  <div
+                    className={style.logInfo}
+                    onClick={() =>
+                      openHistoryItem === index
+                        ? setOpenHistoryItem(null)
+                        : setOpenHistoryItem(index)
+                    }
+                    style={
+                      openHistoryItem === index
+                        ? { backgroundColor: '#333' }
+                        : {}
+                    }
+                  >
+                    <span>
+                      {`Avslutad körning för ${
+                        logEnd.booking_user.pretty_name
+                      }`}
+                    </span>
+                    <span className={style.date}>
+                      {logEnd.logging_date.split('T')[0]}
+                    </span>
+                  </div>
+
+                  <div>
+                    {openHistoryItem === index && (
+                      <div className={style.table}>
+                        <div>
+                          <span>Bilen bokad av</span>
+                          <span>{` ${logEnd.booking_user.pretty_name}`}</span>
+                        </div>
+
+                        <div>
+                          <span>Loggad av</span>
+                          <span>{` ${logEnd.logging_user.pretty_name}`}</span>
+                        </div>
+
+                        <div>
+                          <span>Loggning gjord</span>
+                          <span>
+                            {` ${logEnd.logging_date.split('T')[0]}, kl
+                              ${
+                                logEnd.logging_date.split('T')[1].split('.')[0]
+                              }`}
+                          </span>
+                        </div>
+
+                        <div>
+                          <span>Mätarställning</span>
+                          <span>{` ${logEnd.end_km} km`}</span>
+                        </div>
+
+                        <div>
+                          <span>Lämnat meddelande</span>
+                          <span>{` ${logEnd.end_message}`}</span>
+                        </div>
+
+                        <div>
+                          <span>Bilen städades</span>
+                          <span>
+                            {` ${logEnd.end_car_cleaned ? 'Ja' : 'Nej'}`}
+                          </span>
+                        </div>
+
+                        <div style={{ padding: '5px' }}>
+                          Information om tillhörande start-loggning:
+                        </div>
+
+                        <div>
+                          <span>Start-loggning gjord</span>
+                          <span>
+                            {` ${
+                              logEnd.log_start.logging_date.split('T')[0]
+                            }, kl
+                              ${
+                                logEnd.log_start.logging_date
+                                  .split('T')[1]
+                                  .split('.')[0]
+                              }`}
+                          </span>
+                        </div>
+
+                        <div>
+                          <span>Mätarställning vid start-loggning</span>
+                          <span>{` ${logEnd.log_start.start_km} km`}</span>
+                        </div>
+
+                        {logEnd.log_start.logging_user && (
+                          <div>
+                            <span>Start-loggningen gjordes av</span>
+                            <span>
+                              {` ${logEnd.log_start.logging_user.pretty_name}`}
+                            </span>
+                          </div>
+                        )}
+
+                        {logEnd.log_start.logging_user && (
+                          <div>
+                            <span>Meddelande vid start-loggning</span>
+                            <span>{` ${logEnd.log_start.start_message}`}</span>
+                          </div>
+                        )}
                       </div>
                     )}
                   </div>
