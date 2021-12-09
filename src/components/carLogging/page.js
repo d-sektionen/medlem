402d45f6af81ca7bc7beb46bfd1b3056faadad69
Viktor Holta <viktor.holta@outlook.com>
2021-12-09 20:31:58 +0100

    Checked and rewrote car-logging page.js

--- src/components/carLogging/page.js
+++ src/components/carLogging/page.js
@@ -5,16 +5,16 @@ import { post } from '../request'
 import BigPixels from '../layout/bigPixels'
 import { GridContainer, GridItem } from '../ui/grid'
 import { Checkbox } from '../ui/checkbox'
-import style from '../../scss/carlogging.module.scss'
 import { Button } from '../ui/buttons'
+import style from '../../scss/carlogging.module.scss'
 
 import LoggingHistory from './loggingHistory'
 
 const CarLoggingPage = ({ pageContext: { title } }) => {
-  const [driverLiuId, setDriverLiuId] = useState('')
+  const [bookingLiuId, setBookingLiuId] = useState('')
   const [startStop, setStartStop] = useState('start')
   const [cleanCar, setCleanCar] = useState(false)
-  const [distance, setDistance] = useState()
+  const [kilometers, setKilometers] = useState()
   const [message, setMessage] = useState('')
   const [usedTrailer, setUsedTrailer] = useState(false)
   const [trailerLiuId, setTrailerLiuId] = useState('')
@@ -28,63 +28,56 @@ const CarLoggingPage = ({ pageContext: { title } }) => {
     } else {
       setStatusMessageStyle(style.success)
     }
-    // show response.data.status_text
+
     if (response.data.hasOwnProperty('status_text')) {
       setStatusMessage(response.data.status_text)
     } else {
-      console.log("the response didn't have status_text")
+      setStatusMessage('Response code: ' + response.status)
+      console.log("The response didn't have the property status_text")
     }
   }
 
   const submitStartData = async () => {
-    if (driverLiuId === '') {
+    if (bookingLiuId === '') {
       setStatusMessage('LiU-ID:t får inte vara tomt')
       setStatusMessageStyle(style.error)
-    } else if (distance === undefined || distance === '') {
+    } else if (kilometers === undefined || kilometers === '') {
       setStatusMessage('Miltalet får inte vara tomt')
       setStatusMessageStyle(style.error)
     } else {
-      const logStartData = {
-        kilometers: parseInt(distance, DECIMAL_RADIX),
+      const data = {
+        kilometers: parseInt(kilometers, DECIMAL_RADIX),
         message: message,
         car_cleaned: cleanCar,
-        booking_liu_id: driverLiuId,
+        booking_liu_id: bookingLiuId,
       }
 
-      post('/carlogging/starts/', logStartData)
-        .then(response => {
-          updateStatus(response)
-        })
-        .catch(error => {
-          updateStatus(error.response)
-        })
+      post('/carlogging/starts/', data)
+        .then(response => updateStatus(response))
+        .catch(error => updateStatus(error.response))
     }
   }
 
   const submitStopData = async () => {
-    if (driverLiuId === '') {
+    if (bookingLiuId === '') {
       setStatusMessage('LiU-ID:t får inte vara tomt')
       setStatusMessageStyle(style.error)
-    } else if (distance === undefined || distance === '') {
+    } else if (kilometers === undefined || kilometers === '') {
       setStatusMessage('Miltalet får inte vara tomt')
       setStatusMessageStyle(style.error)
     } else {
-      const logData = {
-        kilometers: parseInt(distance, DECIMAL_RADIX),
+      const data = {
+        kilometers: parseInt(kilometers, DECIMAL_RADIX),
         message: message,
         car_cleaned: cleanCar,
-        booking_liu_id: driverLiuId,
+        booking_liu_id: bookingLiuId,
         trailer: usedTrailer,
         trailer_liu_id: trailerLiuId,
       }
 
-      post('/carlogging/entries/', logData)
-        .then(response => {
-          updateStatus(response)
-        })
-        .catch(error => {
-          updateStatus(error.response)
-        })
+      post('/carlogging/entries/', data)
+        .then(response => updateStatus(response))
+        .catch(error => updateStatus(error.response))
     }
   }
 
@@ -92,7 +85,7 @@ const CarLoggingPage = ({ pageContext: { title } }) => {
   useEffect(
     () => {
       if (userData) {
-        setDriverLiuId(userData.username)
+        setBookingLiuId(userData.username)
         setTrailerLiuId(userData.username)
       }
     },
@@ -105,39 +98,32 @@ const CarLoggingPage = ({ pageContext: { title } }) => {
         <GridItem>
           <h2>Ny billoggning</h2>
           <p>
-            Här kan du logga bilkörning. Det gör du både när du börjar eller
-            avslutar din bilkörning. Du kan göra det åt någon annan.
+            Här kan du logga bilkörning för dig eller någon annan. Det gör du
+            både när du börjar och avslutar din bilkörning.
           </p>
           <hr />
 
           <div className={style.inputGroup}>
-            <span>LiU-ID på den som bokat bilen</span>
+            <span>LiU-ID på den som har bokat bilen</span>
             <input
-              value={driverLiuId}
-              onChange={e => {
-                setDriverLiuId(e.target.value)
-              }}
+              value={bookingLiuId}
+              onChange={e => setBookingLiuId(e.target.value)}
             />
           </div>
 
           <div className={style.inputGroup}>
-            <span>Ska du påbörja eller avsluta bilkörningen?</span>
-
+            <span>Ska du påbörja eller avsluta billoggningen?</span>
             <label htmlFor="start-input">
               <input
                 id="start-input"
                 type="radio"
                 name="logtype"
-                value="start"
                 defaultChecked
-                onChange={e => {
-                  setStartStop(e.target.value)
-                  setUsedTrailer(false)
-                }}
+                value="start"
+                onChange={e => setStartStop(e.target.value)}
               />
               Påbörja
             </label>
-
             <label htmlFor="stop-input">
               <input
                 id="stop-input"
@@ -152,78 +138,66 @@ const CarLoggingPage = ({ pageContext: { title } }) => {
 
           <Checkbox
             text={
-              startStop === 'start' ? 'Bilen är städad ' : 'Bilen har städats'
+              startStop === 'start' ? 'Bilen är städad' : 'Bilen har städats'
             }
-            value="cleancar"
+            value="cleanCar"
             click={e => setCleanCar(e.target.checked)}
           />
+
+          {startStop === 'stop' && (
+            <Checkbox
+              text="Släpet har använts"
+              value="trailerUsed"
+              defaultChecked={usedTrailer}
+              click={e => setUsedTrailer(e.target.checked)}
+            />
+          )}
+
+          {startStop === 'stop' && usedTrailer && (
+            <div className={style.inputGroup}>
+              <span>LiU-ID på den som har bokat släpet</span>
+              <input
+                value={trailerLiuId}
+                onChange={e => setTrailerLiuId(e.target.value)}
+              />
+            </div>
+          )}
+
           <div className={style.inputGroup}>
-            <span>Miltal (mätarställning i km)</span>
-            {/* TODO: Lägg till senaste miltalet? Hämta personens senaste logstart's startKm */}
+            <span>Miltal (mätarställning i kilometer)</span>
             <input
               type="number"
               name="mileage"
               placeholder="t.ex. 79472"
               min="0"
               step="1"
-              onChange={e => {
-                setDistance(e.target.value)
-              }}
+              onChange={e => setKilometers(e.target.value)}
             />
           </div>
-          {startStop === 'stop' && (
-            <div className={style.inputGroup}>
-              <Checkbox
-                text="Släpet har använts"
-                click={e => setUsedTrailer(e.target.checked)}
-              />
-            </div>
-          )}
-          {startStop === 'stop' && usedTrailer && (
-            <div className={style.inputGroup}>
-              <span>LiU-ID på den som bokat släpet</span>
-              <input
-                value={trailerLiuId}
-                onChange={e => {
-                  setTrailerLiuId(e.target.value)
-                }}
-              />
-            </div>
-          )}
 
           <div className={style.inputGroup}>
-            <span>Valfritt meddelande</span>
-
-            <textarea
-              onChange={e => {
-                setMessage(e.target.value)
-              }}
-            />
+            <span>Meddelande (valfritt)</span>
+            <textarea onChange={e => setMessage(e.target.value)} />
           </div>
 
+          <hr />
           <p>
             Se till att du klickat i alla rutor som stämmer för loggningen.
-            Observera att den du loggar åt (om du loggar åt någon annan) kommer
-            att kunna se det du skrivit, och att det är just du som gjort det.
+            Observera att den du loggar åt kommer att kunna se det du skrivit,
+            och att det är just du som har gjort det.
           </p>
 
           <div className={statusMessageStyle}>{statusMessage}</div>
           <Button
-            onClick={() => {
-              if (startStop === 'start') {
-                submitStartData()
-              } else {
-                submitStopData()
-              }
-            }}
+            onClick={() =>
+              startStop === 'start' ? submitStartData() : submitStopData()
+            }
           >
             {startStop === 'start'
               ? 'Påbörja bilkörning'
               : 'Avsluta bilkörning'}
           </Button>
-          <br />
         </GridItem>
-
         <GridItem>
           <LoggingHistory />
         </GridItem>
@@ -231,4 +205,5 @@ const CarLoggingPage = ({ pageContext: { title } }) => {
     </BigPixels>
   )
 }
+
 export default CarLoggingPage
