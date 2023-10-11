55911388d178c712cc946eec9f95ee3da195e98c
wind1s <44732263+wind1s@users.noreply.github.com>
2023-10-12 00:22:33 +0200

    QrScanner improvment: Changed the qrscanner to stay open for successive scans. Updated the layout to accomodate for removing modal popup and placing the camera view inline. Removed the shift functionality for swapping action as it complicated the qrscanner update.

--- src/components/checkin/qrScanner.js
+++ src/components/checkin/qrScanner.js
@@ -1,25 +1,43 @@
 import React, { useState, useRef, useEffect } from 'react'
 
 import { BrowserQRCodeReader } from '@zxing/library'
-import { useCloseModal } from '../modal/useModal'
 
-const QrScanner = ({ onSubmit }) => {
+const QrScanner = ({ onSubmit, refresh }) => {
   const videoElement = useRef(null)
-  const close = useCloseModal()
+  const [qrScannerState, setQrScannerState] = useState(false)
+  // TODO: Swap zxing/library for zxing/browser as this class was moved there.
   const codeReader = new BrowserQRCodeReader()
 
+  const reloadQrScanner = () => {
+    setTimeout(() => {
+      codeReader.reset()
+      setQrScannerState(!qrScannerState)
+    }, 1500)
+  }
+
   useEffect(() => {
-    codeReader
-      .decodeFromInputVideoDevice(undefined, videoElement.current)
-      .then(result => {
-        onSubmit({ text: result.text })
-        close()
-      })
-      .catch(err => console.error(err))
+    setTimeout(() => {
+      codeReader
+        .decodeOnceFromVideoDevice(undefined, videoElement.current)
+        .then(result => {
+          onSubmit({ text: result.text })
+          reloadQrScanner()
+        })
+        .catch(err => console.error(err))
+    }, 500)
 
     return () => codeReader.reset()
-  }, [])
-  return <video ref={videoElement} muted style={{ display: 'block' }} />
+  }, [qrScannerState, refresh])
+
+  return (
+    <video
+      ref={videoElement}
+      muted
+      style={{ display: 'block' }}
+      width="100%"
+      height="auto"
+    />
+  )
 }
 
 export default QrScanner
