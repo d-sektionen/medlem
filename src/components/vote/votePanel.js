90c76e4d838c5ef173bd79108571843566d74835
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2023-11-04 13:46:19 +0100

    Add ability to remove vote
    * Also displays errors on the attendee voting panel.

--- src/components/vote/votePanel.js
+++ src/components/vote/votePanel.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import useSWR from 'swr'
 import VoteForm from './voteForm'
+import style from '../../scss/vote.module.scss'
 
 // import style from '../../scss/vote.module.scss'
 
@@ -8,21 +9,24 @@ const VotePanel = ({ meeting }) => {
   const { data: votes, error, revalidate } = useSWR(
     () => `/voting/votes/?meeting_id=${meeting.id}`
   )
+  const [errors, setErrors] = useState({})
+  const setFormErrors = errors => {
+    setErrors(errors)
+    setTimeout(() => setErrors({}), 3000)
+  }
 
-  useEffect(
-    () => {
-      revalidate()
-    },
-    [meeting]
-  )
+  useEffect(() => {
+    revalidate()
+  }, [meeting])
   return (
     <div>
       <h2>Rösta</h2>
+      {errors && <div className={style.formError}>{errors.voteError}</div>}
       {votes && (
         <>
           {votes.length === 0 && <p>Det finns ingen aktiv omröstning</p>}
           {votes.map(vote => (
-            <VoteForm key={vote.id} vote={vote} />
+            <VoteForm key={vote.id} vote={vote} setErrors={setFormErrors} />
           ))}
         </>
       )}
