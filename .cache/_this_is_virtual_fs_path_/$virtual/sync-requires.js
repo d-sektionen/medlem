f2475d80f38b4bf5b5bec75be50ecdc77a82e875
Therese Selberg <these002@student.liu.se>
2024-02-26 19:34:17 +0100

    testar att börja från scratch
    Co-authored-by: CitronMelon <CitronMelon@users.noreply.github.com>

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,21 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\.cache\\dev-404-page.js")),
+  "component---src-components-404-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\404Page.js")),
+  "component---src-components-attendance-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\attendance\\page.js")),
+  "component---src-components-booking-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\booking\\page.js")),
+  "component---src-components-checkin-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\checkin\\page.js")),
+  "component---src-components-home-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\homePage.js")),
+  "component---src-components-keylog-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\keylogPage.js")),
+  "component---src-components-netlight-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\netlightPage.js")),
+  "component---src-components-preferences-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\preferences\\page.js")),
+  "component---src-components-vote-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\vote\\page.js")),
+  "component---src-components-voting-admin-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\votingAdmin\\page.js")),
+  "component---src-components-voting-counter-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\votingCounter\\page.js")),
+  "component---src-components-voting-guest-page-js": preferDefault(require("C:\\Users\\tiree\\code\\webbu\\medlem\\src\\components\\votingGuest\\page.js"))
+}
+
