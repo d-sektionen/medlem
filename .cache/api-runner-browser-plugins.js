b05e27da36d61a4b46e9cd25cf7d93640b3902c7
Therese Selberg <these002@student.liu.se>
2024-03-04 17:11:41 +0100

    fixar fuckup med cachefiler

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"Datateknologsektionen","short_name":"D-sektionen","start_url":"/","background_color":"#FFFFFF","theme_color":"#20407c","display":"standalone","icon":"src/images/pixels.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"e45bfee0c4a81abcafbc97c4c72efb51"},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
