1059b43844affe971c59b215eb501566c15591ac
Samuel Rothin <samuel.rothin@gmail.com>
2024-10-07 19:50:56 +0200

    Fixed crash with loading subscriptions

--- src/components/preferences/addCalendarSubscription.js
+++ src/components/preferences/addCalendarSubscription.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import AutoForm from '../form/form'
+import useSWR from 'swr'
 
 const AddCalendarSubscription = ({ create }) => {
   const { data, mutate } = useSWR('/booking/items/')
-} 
-return (
-  <AutoForm
-    endpoint="/account/calendar-subscriptions/"
-    customFetcher={create}
-    defaults={{
-      include_bookings: true,
-      include_events_attending: true,
-      include_events_not_attending: true,
-      include_bookable_items: [1,2,3,4,5]
-    }}
-  />
-)
+
+  return (
+    <AutoForm
+      endpoint="/account/calendar-subscriptions/"
+      customFetcher={create}
+      defaults={{
+        include_bookings: true,
+        include_events_attending: true,
+        include_events_not_attending: true,
+        include_bookable_items: [1,2,3,4,5]
+      }}
+    />
+  )
+
+}
 
 export default AddCalendarSubscription
