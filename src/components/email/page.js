c42b2520c6fe6cdb685ab03459c1915aca394e35
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2025-04-24 19:09:03 +0200

    Rework mail page to work with new backend implementation

--- src/components/email/page.js
+++ src/components/email/page.js
@@ -1,142 +1,106 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import { Button } from '../ui/buttons'
 import { GridContainer, GridItem } from '../ui/grid';
 import BigPixels from '../layout/bigPixels';
 import TitleChooser from '../ui/titleChooser';
 import Editor from 'react-simple-wysiwyg';
-import { emailBoxes } from '../../scss/email.module.scss';
+import { emailBoxes, previewBox } from '../../scss/email.module.scss';
 import useSWR from 'swr'
+import { post } from '../request'
+import DateTimePicker from '../form/dateTimePicker';
 
 /*
  * EmailPage for sending emails defined by templates in the backend.
  */
 
 const EmailPage = ({ pageContext: { title } }) => {
-    const [emailType, setEmailType] = useState(null)
-    const [currentPreview, setCurrentPreview] = useState('')
-    const { data: emailTypes } = useSWR('/email/templates/')
-    /* const { data: emailTypes } = {
-        data: [
-            { id: 1, name: 'Infomejl', category: 'InfU', subject: "Infomejl - Vecka 21", description: 'Den första rutan är till för infochefens hörna. Du skriver en titel och sen en text. Rutan under är till för själva nyheterna och då kan du skriva titel för varje ny nyhet och text under titlarna.' },
-            { id: 2, name: 'Sektionsmöte', category: 'Styrelse', subject: "Kallelse till sektionsmöte", description: 'Skicka kallelse om sektionsmöte' }
-        ]
-    }
-    */
-    const [subject, setSubject] = useState(emailType.subject || '');
-    const [htmlContent, setHtmlContent] = useState('');
-    /* Schedule email */
-    const [sendAt, setSendAt] = useState('');
-    const [sendTo, setSendTo] = useState('');
-    const [showEmailInput, setShowEmailInput] = useState(false);
+  const [emailType, setEmailType] = useState(null)
+  const [subject, setSubject] = useState(emailType?.subject || '');
+  const [htmlContent, setHtmlContent] = useState('');
+  const [sendAt, setSendAt] = useState(new Date());
 
-    const categorizedItems = emailTypes
+  const { data: emailTypes } = useSWR('/mail/mail-template/')
+
+  const categorizedItems = emailTypes
     ? emailTypes.reduce((accumulator, itm) => {
-        const cat = itm.category || 'Okategoriserat'
-        if (Object.prototype.hasOwnProperty.call(accumulator, cat)) {
-            return {
-                ...accumulator,
-                [cat]: [...accumulator[cat], itm],
-            }
+      const cat = itm.category || 'Okategoriserat'
+      if (Object.prototype.hasOwnProperty.call(accumulator, cat)) {
+        return {
+          ...accumulator,
+          [cat]: [...accumulator[cat], itm],
         }
-            return { ...accumulator, [cat]: [itm] }
-        }, {})
+      }
+      return { ...accumulator, [cat]: [itm] }
+    }, {})
     : {}
 
-    const { data: previews, mutate } = useSWR(
-        () =>
-            emailType &&
-            `/email/?emails=${emailType.id}`
-    )
+  const handleEmailTypeChange = (type) => {
+    setHtmlContent(type.template)
+    setEmailType(type)
+  }
 
-    const create = async data => {
-        const { data: newEmail } = await post('/email/emails/', data)
-        mutate([...preview, newEmail])
-        return newEmail
-    }
-
-    const handleSendEmail = () => {
-        const emailData = {
-            subject: subject,
-            type: emailType.id,
-            html: htmlContent,
-            sendAt: sendAt,
-            sendTo: sendTo || undefined, // Include sendTo if specified
-        };
-        create(emailData);
+  const handleSendEmail = async () => {
+    const emailData = {
+      subject: subject,
+      category: emailType.category,
+      html: htmlContent,
+      send_at: sendAt,
     };
 
-    return (
-        <BigPixels>
-            <GridContainer>
-                <GridItem fullWidth>
-                    <TitleChooser
-                        title={title}
-                        choice={emailType}
-                        setChoice={setEmailType}
-                        categorizedChoices={categorizedItems}
-                        label="name"
-                    />
-                </GridItem>
-                {emailType && (
-                    <>
-                        <GridItem fullWidth>
-                            <div className={emailBoxes}>
-                                <h2>{emailType.name}</h2>
-                                <p>{emailType.description}</p>
-                            </div>
-                            <div className={emailBoxes}>
-                                <label>Här kan du skriva ämnet till mejlet, din typ av mejl kan ha ett förslag på ämne redan:</label>
-                                <input type='text' value={subject} onChange={setSubject} required/>
-                            </div>
-                            <div className={emailBoxes}>
-                                <label>Innehåll:</label>
-                                <Editor value={htmlContent} onChange={(e) => setHtmlContent(e.target.value)} />
-                                <textarea value={htmlContent} onChange={(e) => setHtmlContent(e.target.value)} />
-                            </div>
-                            <div className={emailBoxes}>
-                                <button onClick={() => setShowEmailInput(!showEmailInput)}>
-                                    {showEmailInput ? 'Göm anpassad mejllista' : 'Bestäm anpassad mejllista'}
-                                </button>
-                                {showEmailInput && (
-                                    <>
-                                        <label>Mottagare</label>
-                                        <p>En eller flera emailadresser, separerade med kommatecken (Om du bestämmer en mejllista här så kommer backend ignorera sin egen mejllista)</p>
-                                        <input type='text' value={sendTo} onChange={(e) => setSendTo(e.target.value)} />
-                                    </>
-                                )}
-                            </div>
-                            <div className={emailBoxes}>
-                                <label>Skicka vid:</label>
-                                <input type='datetime-local' value={sendAt} onChange={(e) => setSendAt(e.target.value)} required />
-                                <button onClick={handleSendEmail}>
-                                    Skicka {emailType.name}
-                                </button>
-                            </div>
-                        </GridItem>
-                        <GridItem fullWidth>
-                            <div className={emailBoxes}>
-                                <h2>Preview</h2>
-                                <p>
-                                    Här kan du se redan publicerade mejls. Även de som ännu inte skickats. Klicka på en preview nedan för att få upp den längre ner.
-                                    Detta kan liknas med julkalendern där admins kan se luckor i förväg.
-                                </p>
-                                <ul>
-                                    {previews && previews.length ? (
-                                        previews.map(preview => (
-                                            <li style={{textDecoration: 'underline'}} key={preview.id} onClick={() => setCurrentPreview(preview.html)}>{preview.name}</li>
-                                        ))
-                                    ) : (
-                                        <li style={{fontWeight: 'bold'}}>Inga previews tillgängliga</li>
-                                    )}
-                                </ul>
-                                <hr/>
-                                <div dangerouslySetInnerHTML={{ __html: currentPreview }} />
-                            </div>
-                        </GridItem>
-                    </>
-                )}
-            </GridContainer>
-        </BigPixels>
-    )
+    await post('/mail/mail/send/', emailData)
+  };
+
+  const handleSendPreview = async () => {
+    const emailData = {
+      subject: subject,
+      category: emailType.category,
+      html: htmlContent,
+      send_at: sendAt,
+    };
+
+    await post('/mail/mail/send_sample/', emailData)
+  }
+
+  return (
+    <BigPixels>
+      <GridContainer>
+        <GridItem fullWidth>
+          <TitleChooser
+            title={title}
+            choice={emailType}
+            setChoice={handleEmailTypeChange}
+            categorizedChoices={categorizedItems}
+            label="name"
+          />
+        </GridItem>
+        {emailType && (
+          <>
+            <GridItem fullWidth>
+              <div className={emailBoxes}>
+                <h2>{emailType.name}</h2>
+              </div>
+              <div className={emailBoxes}>
+                <label>Här kan du skriva ämnet till mejlet, din typ av mejl kan ha ett förslag på ämne redan:</label>
+                <input type='text' value={subject} onChange={(e) => setSubject(e.target.value)} required />
+              </div>
+              <div className={emailBoxes}>
+                <label>Innehåll:</label>
+                <Editor value={htmlContent} onChange={(e) => setHtmlContent(e.target.value)} />
+              </div>
+              <div className={emailBoxes}>
+                <label>Skicka vid:</label>
+                <DateTimePicker value={sendAt} onChange={setSendAt}></DateTimePicker>
+              </div>
+              <div className={emailBoxes}>
+                <Button onClick={handleSendEmail}>Skicka {emailType.name}</Button>
+                <Button onClick={handleSendPreview}>Skicka förhandsgranskning</Button>
+              </div>
+            </GridItem>
+          </>
+        )}
+      </GridContainer>
+    </BigPixels>
+  )
 }
 
 export default EmailPage
