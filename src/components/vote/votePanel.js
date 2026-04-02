e845100ec510aa9ec2b41f1c5f22bca42962ad28
Markus Piotrowski <markuspiotrowski@live.com>
2026-04-02 19:48:56 +0200

    Votes using sockets

--- src/components/vote/votePanel.js
+++ src/components/vote/votePanel.js
@@ -1,16 +1,97 @@
 import React, { useEffect, useState } from 'react'
-import useSWR from 'swr'
 import VoteForm from './voteForm'
 import { formError } from '../../scss/vote.module.scss'
+import backendService from '../request/backendService'
+import socket, { joinRoom } from '../request/socket'
 
 const VotePanel = ({ meeting }) => {
-  const { data: votes = [], mutate } = useSWR(
-    `/voting/votes/?meeting_id=${meeting.id}`
-  )
+  const [votes, setVotes] = useState([])
+
+  async function fetchVotes() {
+    if (meeting) {
+      const resp = await backendService.get(
+        `/voting/votes/?meeting_id=${meeting.id}`
+      )
+      setVotes(resp.data)
+      console.log('Fetched votes', resp.data)
+    }
+  }
 
   useEffect(() => {
-    mutate()
-  }, [meeting, mutate])
+    fetchVotes()
+  }, [meeting])
+
+  joinRoom(`meeting_votes_${meeting.id}`)
+
+  socket.on('new_vote', (data) => {
+    if (data.meeting === meeting.id) {
+      if (votes.find((v) => v.id === data.id)) {
+        votes[votes.findIndex((v) => v.id === data.id)] = data
+        setVotes([...votes])
+        return
+      }
+      votes.push(data)
+      setVotes([...votes])
+    }
+  })
+
+  socket.on('delete_vote', (data) => {
+    if (data.meeting === meeting.id) {
+      const newVotes = votes.filter((v) => v.id !== data.id)
+      setVotes(newVotes)
+    }
+  })
+
+  socket.on('delete_alternative', (data) => {
+    if (data.meeting === meeting.id) {
+      const newVotes = votes.map((v) => {
+        if (v.id === data.vote) {
+          return {
+            ...v,
+            alternatives: v.alternatives.filter((a) => a.id !== data.id),
+          }
+        }
+        return v
+      })
+      setVotes(newVotes)
+    }
+  })
+
+  socket.on('new_alternative', (data) => {
+    if (data.meeting === meeting.id) {
+      const newVotes = votes.map((v) => {
+        if (v.id === data.vote) {
+          return {
+            ...v,
+            alternatives: [...v.alternatives, data],
+          }
+        }
+        return v
+      })
+      setVotes(newVotes)
+    }
+  })
+
+  socket.on('update_alternative', (data) => {
+    if (data.meeting === meeting.id) {
+      const newVotes = votes.map((v) => {
+        if (v.id === data.vote) {
+          return {
+            ...v,
+            alternatives: v.alternatives.map((a) =>
+              a.id === data.id ? data : a
+            ),
+          }
+        }
+        return v
+      })
+      setVotes(newVotes)
+    }
+  })
+
+  // const { data: votes = [], mutate } = useSWR(
+  //   `/voting/votes/?meeting_id=${meeting.id}`
+  // )
 
   const [errors, setErrors] = useState({})
   const setFormErrors = (errors) => {
