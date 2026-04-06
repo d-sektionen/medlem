b214b69113f0d0ba9c2a92679bb44c95c01cb469
Markus Piotrowski <markuspiotrowski@live.com>
2026-04-06 19:16:34 +0200

    Update speaker panel sockets

--- src/components/votingAdmin/speakerPanel.js
+++ src/components/votingAdmin/speakerPanel.js
@@ -4,7 +4,7 @@ import backendService from '../request/backendService'
 import { FiTrash2 } from 'react-icons/fi'
 import { List, ListButton, ListItem } from '../ui/list'
 
-import socket, { joinRoom } from '../request/socket'
+import socket, { joinRoom, leaveRoom } from '../request/socket'
 
 const SpeakerPanel = ({ meeting }) => {
   const [speakers, setSpeakers] = useState([])
@@ -20,28 +20,35 @@ const SpeakerPanel = ({ meeting }) => {
 
   useEffect(() => {
     handleMeetingChange()
-  }, [meeting])
+    socket.on('connect', () => {
+      handleMeetingChange()
+    })
 
-  joinRoom(`meeting_speker_${meeting.id}`)
+    joinRoom(`meeting_speker_${meeting.id}`)
 
-  socket.on('new_speaker_request', (data) => {
-    if (data.meeting_id === meeting.id) {
-      if (speakers.find((s) => s.id === data.speaker.id)) {
-        return
-      }
-      speakers.push(data.speaker)
-      setSpeakers([...speakers])
-    }
-  })
+    socket.on('new_speaker_request', (data) => {
+      if (data.meeting_id !== meeting.id) return
+      if (speakers.find((s) => s.id === data.speaker.id)) return
 
-  socket.on('delete_speaker_request', (data) => {
-    if (data.meeting_id === meeting.id) {
-      const newSpeakers = speakers.filter(
-        (s) => s.id !== data.speaker_request_id
+      setSpeakers((prev) => [...prev, data.speaker])
+    })
+
+    socket.on('delete_speaker_request', (data) => {
+      if (data.meeting_id !== meeting.id) return
+
+      setSpeakers((prev) =>
+        prev.filter((s) => s.id !== data.speaker_request_id)
       )
-      setSpeakers(newSpeakers)
+    })
+
+    return () => {
+      socket.off('connect', handleMeetingChange)
+      socket.off('new_speaker_request')
+      socket.off('delete_speaker_request')
+
+      leaveRoom(`meeting_speker_${meeting.id}`)
     }
-  })
+  }, [meeting])
 
   async function deleteSpeaker(speakerId) {
     await backendService.delete(`/voting/speakers/${speakerId}`)
