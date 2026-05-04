536a03c9943f37998d59156f83411afa3bded99e
Otto Roming <otro@lysator.liu.se>
2026-05-04 19:47:59 +0200

    remove Component import

--- src/components/vote/voteForm.js
+++ src/components/vote/voteForm.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import { post } from '../request'
 import { Button } from '../ui/buttons'
 import { MdOutlineHowToVote } from 'react-icons/md'
