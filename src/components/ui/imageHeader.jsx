ffad2d4bf08ee14731ab94f4ad6eea0399dbf125
Bo Rodin <bobbo.nidor@gmail.com>
2026-09-10 14:05:43 +0200

    Revert one more style import

--- src/components/ui/imageHeader.jsx
+++ src/components/ui/imageHeader.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from '../../scss/ui.module.scss'
+import { imageHeader, Image, gradient } from '../../scss/ui.module.scss'
 import Pattern from './pattern'
 
 const ImageHeader = ({ TitleTag = 'h1', title = '', image = null }) => {
   return (
-    <div className={styles.imageHeader}>
+    <div className={imageHeader}>
       {image ? (
-        <div className={styles.Image} style={{ backgroundImage: `url(${image})` }} />
+        <div className={Image} style={{ backgroundImage: `url(${image})` }} />
       ) : (
         <Pattern seed={title} />
       )}
-      <div className={styles.gradient} />
-      <TitleTag className={styles.Title}>{title}</TitleTag>
+      <div className={gradient} />
+      <TitleTag>{title}</TitleTag>
     </div>
   )
 }
