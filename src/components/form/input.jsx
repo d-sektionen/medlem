d-sektionen/medlem

commit fde6bf4abbe5f4d1e506bb3be14bbcbaccf7bdda

fix lints for form

src/components/form/input.jsx
@@ -1,4 +1,4 @@
-import React, { useDeferredValue, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import DateTimePicker from "./dateTimePicker";
 
 const AutoInput = ({
@@ -19,7 +19,7 @@ const AutoInput = ({
     if (value) {
       setInitialOptions(value);
     }
-  }, []); // Run only once, when the component mounts
+  }, [value]); // Run only once, when the component mounts
 
   const change = (e) => {
     onChange(e.target.value);
@@ -78,6 +78,7 @@ const AutoInput = ({
     map[type]
   ) : (
     <input
+    id="input-input"
       value={value}
       onChange={change}
       maxLength={max_length}
@@ -86,7 +87,8 @@ const AutoInput = ({
   );
 
   return (
-    <label>
+    <label
+      htmlFor="input-input">
       {`${label}`}
       {required && <span>*</span>}
       <div>{component}</div>

src/components/form/dateTimePicker.jsx
@@ -6,7 +6,6 @@ import {
   setMonth,
   setYear,
 } from "date-fns";
-import React, { useState } from "react";
 
 const DateTimePicker = ({ value, onChange, required = false }) => {
   return (

src/components/form/form.jsx
@@ -19,10 +19,6 @@ const AutoForm = ({
     setValues((old) => ({ ...old, [field]: value }));
   };
 
-  const setError = (field, error) => {
-    setErrors((old) => ({ ...old, [field]: error }));
-  };
-
   useEffect(() => {
     // request endpoint info from server
     options(endpoint)
@@ -38,8 +34,8 @@ const AutoForm = ({
 
         setFields(editable);
       })
-      .catch((err) => {});
-  }, endpoint);
+      .catch((_err) => {});
+  }, [endpoint]);
 
   return (
     <form
@@ -57,8 +53,7 @@ const AutoForm = ({
         }
       }}
     >
-      {fields &&
-        fields.map((field) => (
+      {fields?.map((field) => (
           <React.Fragment key={field.key}>
             <AutoInput
               {...field}

commit 067aa1b5f8fcbab61d5178dcab846559b997c11f

revert broken changes from fde6bf4

src/components/form/input.jsx
@@ -19,7 +19,7 @@ const AutoInput = ({
     if (value) {
       setInitialOptions(value);
     }
-  }, [value]); // Run only once, when the component mounts
+  }, []); // Run only once, when the component mounts
 
   const change = (e) => {
     onChange(e.target.value);