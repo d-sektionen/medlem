81c0e5ffdc1e5ccf12b092e1731155d6770b4523
Otto Roming <otro@lysator.liu.se>
2026-09-25 23:40:15 +0200

    ignore mutate

--- src/components/preferences/addCalendarSubscription.jsx
+++ src/components/preferences/addCalendarSubscription.jsx
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import AutoForm from "../form/form";
 
 const AddCalendarSubscription = ({ create }) => {
-  const { data, mutate } = useSWR("/booking/item-pools/");
+  const { data, mutate: _ } = useSWR("/booking/item-pools/");
 
   const defaults = {
     include_bookings: false,
